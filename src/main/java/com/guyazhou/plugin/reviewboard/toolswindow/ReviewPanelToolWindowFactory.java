f46f3e677a4758d668ee020dee5cfe494198324f
Yaalon <asiaon123@gmail.com>
2020-05-30 19:44:37 +0800

    Add support for idea2020

--- src/main/java/com/guyazhou/plugin/reviewboard/toolswindow/ReviewPanelToolWindowFactory.java
+++ src/main/java/com/guyazhou/plugin/reviewboard/toolswindow/ReviewPanelToolWindowFactory.java
@@ -1,8 +1,11 @@
 package com.guyazhou.plugin.reviewboard.toolswindow;
 
+import com.guyazhou.plugin.reviewboard.toolswindow.allpr.AllReviewRequestToolWindow;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,7 +16,14 @@ public class ReviewPanelToolWindowFactory implements ToolWindowFactory {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+        AllReviewRequestToolWindow allReviewRequestToolWindow = new AllReviewRequestToolWindow();
+
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(allReviewRequestToolWindow.getContent(), "Test", false);
+
         ContentManager contentManager = toolWindow.getContentManager();
+        contentManager.addContent(content);
     }
 
 }
---
+++ src/main/java/com/guyazhou/plugin/reviewboard/toolswindow/allpr/AllReviewRequestToolWindow.java
@@ -0,0 +1,17 @@
+package com.guyazhou.plugin.reviewboard.toolswindow.allpr;
+
+import javax.swing.*;
+
+/**
+ * @author Yaalon 2020/4/25
+ */
+public class AllReviewRequestToolWindow {
+    private JPanel panel1;
+    private JTextField heelloTextField;
+    private JPasswordField passwordField1;
+
+    public JPanel getContent() {
+        return panel1;
+    }
+
+}
